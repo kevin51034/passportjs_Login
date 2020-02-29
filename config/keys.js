e4519a84380da3ecc8fbd936f8c9dd7a8cbed654
陳文擎 <kevin51034@gmail.com>
2020-03-01 01:01:44 +0800

    add keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-//dbPassword = 'mongodb://kevin51034:'+ encodeURIComponent('password') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+//dbPassword = 'mongodb://kevin51034:'+ encodeURIComponent('JnXNR7@xxc2UFz9') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
 
 module.exports = {
-    mongoURI: 'mongodb+srv://kevin51034:password@cluster0-nzrgh.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://kevin51034:JnXNR7@xxc2UFz9@cluster0-nzrgh.mongodb.net/test?retryWrites=true&w=majority'
 };
