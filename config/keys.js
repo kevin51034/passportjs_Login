42f4bc0aa6f34d3a28f0a6c5909f6389c6e87dee
陳文擎 <kevin51034@gmail.com>
2020-02-29 22:53:17 +0800

    keys commit test

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+//dbPassword = 'mongodb://kevin51034:'+ encodeURIComponent('9n7HgYorTWNKZykP') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+
+module.exports = {
+    mongoURI: 'mongodb+srv://kevin51034:9n7HgYorTWNKZykP@cluster0-nzrgh.mongodb.net/test?retryWrites=true&w=majority'
+};
